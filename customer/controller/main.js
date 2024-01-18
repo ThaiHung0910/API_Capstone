48144f0b87492808cf3d2e466c4c5cddbaf12dbd
ThaiHung <thaihung147741@gmail.com>
2024-01-18 22:44:25 +0700

    commit

--- customer/controller/main.js
+++ customer/controller/main.js
@@ -1,19 +1,39 @@
 
 
+function renderProduct(arrProductList) {
+    var content = "";
+    for (var i = 0; i < arrProductList.length; i++) {
+      var arr = arrProductList[i];
+      content += `
+      <div class="card">
+          <img src="${arr.img}" class="card-img-top" alt="...">
+          <div class="card-body">
+              <h5 class="card-title">${arr.name}</h5>
+              <p class="card-text">${arr.desc}</p>
+              <div class="purchase">
+                  <p class="product-price">${arr.price}</p>
+                  <span class="btn btn-primary btn-add">Add</span>
+              <div>
+          </div>
+      </div>`;
+    }
+    document.getElementById("phoneList").innerHTML = content;
+}
+
 function fetchProductList() {
-    axios({
-        url: 'https://6597f7c2668d248edf23d04d.mockapi.io/api_capstone',
-        method: "GET"
+  axios({
+    url: "https://6597f7c2668d248edf23d04d.mockapi.io/api_capstone",
+    method: "GET",
+  })
+    .then(function (res) {
+      renderProduct(res.data);
+      console.log(res.data);
     })
-        .then(function(res)  {
-                console.log(res.data);
-              })
-              .catch(function(err)  {
-               console.log(err);
-              });
+    .catch(function (err) {
+      console.log(err);
+    });
+}
 
-}   
-
-fetchProductList()
+fetchProductList();
 
 
