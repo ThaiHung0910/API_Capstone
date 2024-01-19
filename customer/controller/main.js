e6afd87eea3a915d6e8d0f9005661a50631fcad0
ThaiHung <thaihung147741@gmail.com>
2024-01-19 22:55:42 +0700

    commit

--- customer/controller/main.js
+++ customer/controller/main.js
@@ -1,23 +1,32 @@
 
 
 function renderProduct(arrProductList) {
-    var content = "";
+    var content = [];
     for (var i = 0; i < arrProductList.length; i++) {
       var arr = arrProductList[i];
       content += `
-      <div class="card">
-          <img src="${arr.img}" class="card-img-top" alt="...">
-          <div class="card-body">
-              <h5 class="card-title">${arr.name}</h5>
-              <p class="card-text">${arr.desc}</p>
-              <div class="purchase">
-                  <p class="product-price">${arr.price}</p>
-                  <span class="btn btn-primary btn-add">Add</span>
-              <div>
+      <div class="product__content">
+        <div class="product__item card">
+          <img src="${arr.img}" alt="" />
+          <div class="item__text">
+            <h3>${arr.name}<span>$${arr.price}</span></h3>
+            <p>${arr.type}</p>
           </div>
-      </div>`;
+
+          <div class="item__action">
+            <div class="rate">
+              <i class="fa-solid fa-star"></i>
+              <i class="fa-solid fa-star"></i>
+              <i class="fa-solid fa-star"></i>
+              <i class="fa-solid fa-star"></i>
+              <i class="fa-solid fa-star"></i>
+            </div>
+            <button><i class="fa-solid fa-cart-shopping"></i>Add</button>
+          </div>
+        </div>
+      </div>`
     }
-    document.getElementById("phoneList").innerHTML = content;
+    document.getElementById('phoneList').innerHTML = content
 }
 
 function fetchProductList() {
